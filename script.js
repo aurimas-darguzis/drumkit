a8ed3f9db85b80f82eefa449282706da89bd2753
aurimas-darguzis <aurimas@darguzis.com>
2017-12-10 13:49:40 +0000

    update

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let snare = document.getElementById('snare');
-window.addEventListener('keyup', (e) => {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   if (!audio) return
   audio.currentTime = 0
   audio.play()
   key.classList.add('playing')
-})
+}
+
+function removeTransition() {
+  this.classList.remove('playing')
+}
 
 const keys = document.querySelectorAll('.key')
-keys.forEach(key => console.log(key))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+
+window.addEventListener('keyup', playSound)
+
+
+function log(...e) {
+  console.log(...e)
+}
