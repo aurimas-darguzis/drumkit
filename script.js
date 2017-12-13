083152ea7db139b7ac2c37656be9fa85e5a1b8dc
Aurimas <aurimas@darguzis.com>
2017-12-13 18:33:46 +0000

    update

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function playSound(e) {
+
+  let keyCode
+  e.type === 'keyup' ? keyCode = e.keyCode : keyCode = this.getAttribute('data-key')
+
+  
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   if (!audio) return
@@ -7,12 +12,23 @@ function playSound(e) {
   key.classList.add('playing')
 }
 
+function playWithKeyboard(e) {
+  const el = this;
+  const key = el.getAttribute('data-key')
+  const audio = document.querySelector(`audio[data-key="${key}"]`)
+  if (!audio) return
+  audio.currentTime = 0
+  audio.play()
+  this.classList.add('playing')
+}
+
 function removeTransition() {
   this.classList.remove('playing')
 }
 
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('click', playSound))
 
 
 window.addEventListener('keyup', playSound)
