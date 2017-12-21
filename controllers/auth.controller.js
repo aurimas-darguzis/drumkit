21ac5a8944f6da17f8a73456c17295aab21c5867
aurimas-darguzis <aurimas@darguzis.com>
2017-12-21 11:48:39 +0000

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,18 @@
 function AuthController() {
+
   function isAuthorized(roles, neededRole) {
     return roles.indexOf(neededRole) >= 0
   }
+
+  function isAuthorizedAsync(roles, neededRole, cb) {
+    setTimeout(function () {
+      cb(roles.indexOf(neededRole) >= 0)
+    }, 0)
+  }
+
   return {
-    isAuthorized
+    isAuthorized,
+    isAuthorizedAsync
   }
 }
 
