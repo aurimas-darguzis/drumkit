026d3f13b3a7b811790f447d60a043e59a368280
aurimas-darguzis <aurimas@darguzis.com>
2017-12-21 12:46:07 +0000

    update

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,8 +1,14 @@
 const assert = require('assert')
+const should = require('chai').should()
 
 describe('Basic Mocha Test', function () {
   it('should throw errors', function () {
     const obj = {task: 'testing', inProgress: true}
     obj.should.have.property('task').equal('testing')
+  })
+
+  it('should allow testing nulls', function () {
+    const iAmNull = null
+    should.not.exist(iAmNull)
   })
 })
