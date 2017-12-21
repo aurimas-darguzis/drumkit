9e476c7c31c49b79d6d1bf9cb22b9e58c3328c7c
aurimas-darguzis <aurimas@darguzis.com>
2017-12-21 11:01:20 +0000

    update

---
+++ test/controllers/auth.controller.spec.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const authController = require('../../controllers/auth.controller')
+
+describe('AuthController', function () {
+  describe('isAuthorized', function () {
+    it('Should return false if not authorized', function () {
+      assert.equal(false, authController.isAuthorized(['user'], 'admin'))
+    })
+    it('Should return true if authorized', function () {
+      assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'))
+    })
+  })
+})
