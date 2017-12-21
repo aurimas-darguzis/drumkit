2e5c768bdb0fd4f3578135e980f586f3d119dd4c
aurimas-darguzis <aurimas@darguzis.com>
2017-12-21 15:50:56 +0000

    update

--- test/controllers/gitService.spec.js
+++ test/controllers/gitService.spec.js
@@ -9,6 +9,7 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const https = require('https')
+const PassThrough = require('stream').PassThrough
 
 chai.should()
 const gitService = require('')()
@@ -19,12 +20,24 @@ describe('GitService', function () {
       this.request = sinon.stub(https, 'request')
     })
     it('should return user and repos', function () {
+      this.timeout(10000)
+      const gitJson = {login: 'someuser'}
+      const gitResponse = new PassThrough()
+      gitResponse.write(JSON.stringify(gitJson))
+      gitResponse.end()
+
+      // callsArgWith is going to call [1] passing it gitResponse.
+      this.request.callsArgWith(1, gitResponse).returns(new PassThrough())
+
       return gitService.getUser('someuser').then(
         function (user) {
           user.login.should.equal('someuser')
           user.should.have.property('repos')
         }
       )
+    })
+    afterEach(function() {
+      this.request.restore()
     })
   })
 })
